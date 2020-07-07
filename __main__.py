b293d8983228493a9e5535551c118604e0b5fd26
feiger1992 <983232781@qq.com>
2020-07-07 18:43:12 +0800

    push
    Signed-off-by: feiger1992 <983232781@qq.com>

--- __main__.py
+++ __main__.py
@@ -3,6 +3,9 @@ import Metocean
 from Metocean.PYQT import M_window
 from PyQt5.QtWidgets import QApplication
 import sys
+from Metocean.tide import Process_Tide
+from Metocean.current import Single_Tide_Point, Current_pre_process, Read_Report, small_diff_dir
+from Metocean.Wind_and_Wave import Wind_Wave
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
